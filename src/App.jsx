64d35f9aa1cc0910b64b984f6eee43051b5848e0
Aroobs-i <iftikhar8995@gmail.com>
2025-10-05 16:04:33 +0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Hero from "./components/Hero"
 import Highlights from "./components/Highlights"
+import Model from "./components/Model"
 import NavBar from "./components/NavBar"
 
 const App = () =>{
@@ -9,6 +10,7 @@ const App = () =>{
     <NavBar/>
     <Hero/>
     <Highlights/>
+    <Model/>
    </main>
   )
 }
