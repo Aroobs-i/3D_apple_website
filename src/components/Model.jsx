ea4ca3a219a4bead025963d07214b16bbbad3f9e
Aroobs-i <iftikhar8995@gmail.com>
2025-10-05 17:24:31 +0500

    commit

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -4,6 +4,11 @@ import React, { useRef, useState } from 'react'
 import ModelView from './ModelView';
 import { yellowImg } from '../utils';
 import * as THREE from 'three';
+import { Canvas } from '@react-three/fiber';
+import { View } from '@react-three/drei';
+
+import { models, sizes } from "../constants";
+
 
 const Model = () => {
 
@@ -14,11 +19,17 @@ const Model = () => {
         img: yellowImg,
     })
 
+    // modelview camera control
     const cameraControlSmall = useRef();
     const cameraControlLarge = useRef();
 
+    // model
     const small = useRef(new THREE.Group());
+    const large = useRef(new THREE.Group());
 
+    // rotation
+    const [smallRotaion, setSmallRotaion] = useState(0);
+    const [largeRotation, setLargeRotation] = useState(0);
 
     useGSAP(() => {
         gsap.to('#heading', {
@@ -35,7 +46,70 @@ const Model = () => {
             </h1>
             <div className='flex flex-col items-center mt-5'>
                 <div className='w-full h-[75vh] md:h-[90vh] overflow-hidden relative '>
-                    <ModelView/>
+                    <ModelView
+                       index={1}
+                       groupRef={small}
+                       gsapType='view1'
+                       controlRef={cameraControlSmall}
+                       setRotaionState={setSmallRotaion}
+                       item={model}
+                       size={size}
+                    />
+
+                    <ModelView
+                       index={2}
+                       groupRef={large}
+                       gsapType='view2'
+                       controlRef={cameraControlLarge}
+                       setRotaionState={setLargeRotation}
+                       item={model}
+                       size={size}
+                    />
+
+                    <Canvas
+                      className='w-full h-full'
+                      style={{
+                        position: 'fixed', 
+                        top: 0, 
+                        left: 0,
+                        bottom: 0,
+                        right: 0,
+                        overflow: 'hidden',
+                      }}
+                       eventSource={document.getElementById('root')}
+                    >
+                        <View.Port />
+                    </Canvas>
+                </div>
+
+                <div className='mx-auto w-full'>
+                    <p className='text-sm font-light text-center mb-5'>
+                        {model.title}</p>
+                        <div className='flex-center'>
+                            <ul className='color-container'>
+                                {models.map((item, i) => (
+                                    <li 
+                                    key={i} 
+                                    className='w-6 h-6 rounded-full mx-2 cursor-pointer'
+                                    style={{backgroundColor: item.color[0]}}
+                                    onClick={() => setModel(item)}
+                                    />
+                                ))}
+                            </ul>
+                            <button className='size-btn-container'>
+                                {sizes.map(({label, value}) => (
+                                    <span 
+                                    key={label} 
+                                    className='size-btn' 
+                                    style={{backgroundColor: size === value ? 'white' : 'transparent', 
+                                    color: size === value ? 'black' : 'white'}}
+                                    onClick={() => setSize(value)}
+                                    >
+                                        {label}
+                                    </span>
+                                ))}
+                            </button>
+                        </div>
                 </div>
             </div>
         </div>
