6ad24c39af72d57e7d0a0238c3081a0a055b8b6e
Aroobs-i <iftikhar8995@gmail.com>
2025-10-07 17:48:49 +0500

    commit

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -12,11 +12,25 @@ const Features = () => {
     const videoRef = useRef();
 
     useGSAP(() => {
+        gsap.to('#exploreVideo', {
+            scrollTrigger: {
+              trigger: '#exploreVideo',
+              toggleActions: 'play pause reverse restart',
+              start: '-10% bottom',
+            },
+            onComplete: () => {
+                // eslint-disable-next-line no-undef
+                video.play();
+            }
+          })
+
         animateWithGsap('#features_title', { y:0, opacity:1})
         animateWithGsap('.g_grow',
             { scale:1, opacity:1, ease: 'power1' },
             { scrub: 5.5 }
-        )
+        );
+        animateWithGsap('.g_text',
+            { y:0, opacity:1, ease: 'power2.inOut', duration:1 });
     }, [])
 
   return (
@@ -61,6 +75,27 @@ const Features = () => {
                             alt="titanium"
                             className='feature-video g_grow' 
                             />
+                        </div>
+                    </div>
+                    <div className='feature-text-container'>
+                        <div className='flex-1 flex-center'>
+                            <p className='feature-text g_text'>
+                                iPhone 15 Pro is {' '}
+                                <span className='text-white'>
+                                    the first iPhone to feature an aerospace-grade titanium design.
+                                </span>,
+                                using the same alloy used by the spacecraft industry to fly to Mars.
+                            </p>
+                        </div>
+                        <div className='flex-1 flex-center'>
+                            <p className='feature-text g_text'>
+                                Titanium has one of the best strenght-to-weight ratios of any metal,
+                                making these our {' '}
+                                <span className='text-white'>
+                                    lightest Pro models ever.
+                                </span>,
+                                You'll notice the difference the moment you pick one up.
+                            </p>
                         </div>
                     </div>
                 </div>
